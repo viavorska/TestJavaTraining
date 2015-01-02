358e6decbfbf6e90b0cc9f2c343ee1b2f808efa2
Vira Iavorska <vira.iavorska@gmail.com>
2015-01-03 00:30:17 +0200

    тест по изменению группы

--- addressbook-selenium-tests/src/com/example/tests/GroupModificationTests.java
+++ addressbook-selenium-tests/src/com/example/tests/GroupModificationTests.java
@@ -2,10 +2,10 @@ package com.example.tests;
 
 import org.testng.annotations.Test;
 
-public class GroupModificationTest extends TestBase {
+public class GroupModificationTests extends TestBase {
 	
 	@Test
-	public void deletesomeGroup() {
+	public void modifySomeGroup() {
 	    app.getNavigationHelper().openMainPage();
 	    app.getNavigationHelper().goToGroupsPage();
 	    app.getGroupHelper().initGroupModification(1);
