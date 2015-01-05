4d04c4118012e46533d1e3ed84e63260c1dcdf87
Vira Iavorska <vira.iavorska@gmail.com>
2015-01-05 19:59:36 +0200

    Не срабатывает сортировка toLowerCase

--- addressbook-selenium-tests/src/com/example/tests/GroupCreationTests.java
+++ addressbook-selenium-tests/src/com/example/tests/GroupCreationTests.java
@@ -1,6 +1,8 @@
 package com.example.tests;
 
+import java.util.Collections;
 import java.util.List;
+
 import static org.testng.Assert.assertEquals;
 
 import org.testng.annotations.Test;
@@ -15,11 +17,11 @@ public class GroupCreationTests extends TestBase {
     List<GroupData> oldGroupList = app.getGroupHelper().getGroups();
     
     //actions	
-    app.getGroupHelper().initGroupCreation();
     GroupData group = new GroupData();
     group.groupName = "group name 1";
     group.groupHeader = "group header 1";
     group.groupFooter = "group footer 1";
+    app.getGroupHelper().initGroupCreation();    
 	app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupCreation();
     app.getGroupHelper().returnToGroupsPage();
@@ -28,9 +30,11 @@ public class GroupCreationTests extends TestBase {
     List<GroupData> newGroupList = app.getGroupHelper().getGroups();
     
     //compare states
+    //проверку размера списка можно убрать, так как у нас есть проверка объектов списка 
     assertEquals(newGroupList.size(), oldGroupList.size() + 1);
     
     oldGroupList.add(group);
+    Collections.sort(oldGroupList);
     assertEquals(newGroupList, oldGroupList);
   }
   
@@ -38,9 +42,27 @@ public class GroupCreationTests extends TestBase {
   public void testEmptyGroupCreation() throws Exception {
     app.getNavigationHelper().openMainPage();
     app.getNavigationHelper().goToGroupsPage();
+    
+    //save old state
+    List<GroupData> oldGroupList = app.getGroupHelper().getGroups();
+    
+    //actions	
+    GroupData group = new GroupData("", "", "");
     app.getGroupHelper().initGroupCreation();
-    app.getGroupHelper().fillGroupForm(new GroupData("", "", ""));
+    app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupCreation();
     app.getGroupHelper().returnToGroupsPage();
+    
+    //save new state
+    List<GroupData> newGroupList = app.getGroupHelper().getGroups();
+    
+    //compare states
+    //проверку размера списка можно убрать, так как у нас есть проверка объектов списка 
+    assertEquals(newGroupList.size(), oldGroupList.size() + 1);
+    
+    oldGroupList.add(group);
+    Collections.sort(oldGroupList);
+    assertEquals(newGroupList, oldGroupList);
+    
   }     
 }
--- addressbook-selenium-tests/src/com/example/tests/GroupData.java
+++ addressbook-selenium-tests/src/com/example/tests/GroupData.java
@@ -1,7 +1,7 @@
 package com.example.tests;
 
 
-public class GroupData {
+public class GroupData implements Comparable<GroupData> {
 	public String groupName;
 	public String groupHeader;
 	public String groupFooter;
@@ -14,4 +14,42 @@ public class GroupData {
 		this.groupHeader = groupHeader;
 		this.groupFooter = groupFooter;
 	}
+
+	@Override
+	public String toString() {
+		return "GroupData [groupName=" + groupName + "]";
+	}
+
+	//@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((groupName == null) ? 0 : groupName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		GroupData other = (GroupData) obj;
+		if (groupName == null) {
+			if (other.groupName != null)
+				return false;
+		} else if (!groupName.equals(other.groupName))
+			return false;
+		return true;
+	}
+
+	@Override
+	public int compareTo(GroupData other) {
+		return this.groupName.toLowerCase().compareTo(other.groupName.toLowerCase());
+	}	
+	
+	
 }
